5044cc9822e4519781be0abe9fdfe1ef4f05e78e
Kelan Champagne <kelan@yeahrightkeller.com>
2016-08-28 21:04:39 -0700

    Initial Commit

---
+++ SynchronizedExercise/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SynchronizedExercise
+//
+//  Created by Kelan Champagne on 8/28/16.
+//  Copyright © 2016 Kelan Champagne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
